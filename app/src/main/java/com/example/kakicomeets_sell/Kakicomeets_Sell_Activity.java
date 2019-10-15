3fef3261e8fe02267c4a052a36aa91e8e07fc58b
takuya <1911140027r@ed.fuk.kindai.ac.jp>
2019-10-15 18:11:06 +0900

    販売画面のプロジェクトの作成

---
+++ app/src/main/java/com/example/kakicomeets_sell/Kakicomeets_Sell_Activity.java
@@ -0,0 +1,14 @@
+package com.example.kakicomeets_sell;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Kakicomeets_Sell_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.kakicomeets_sell);
+    }
+}
